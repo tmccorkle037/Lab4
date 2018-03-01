37e862fbe7b0159b6c37b1c02f1952d72fd768fe
tmccorkle037 <tmmhc6@mail.missouri.edu>
2018-03-01 12:08:45 -0600

    sets up the program

---
+++ Lab4_2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	ifstream inS;
+	ofstream outS;	
+	string fileName;
+	int n, length, max;
+
+	n = argc;
+	if( n <= 1){
+		cout << "Incorrect command line format" << endl;
+		return 1;
+	}
+	for( int i = 0; i < n; i++ ){	//go through the command line and get all the info for each arg
+		if(		
+	
+	}
+
+	//sprintf(filename, "Raw_data_%02d.txt", 
+	
+	 
+
+	return 0;
+
+}
